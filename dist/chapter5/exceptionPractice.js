2619441c4cf67bb8f652eb5bafe872e7fc69616d
nogtk <taka.naoga@moneyforward.co.jp>
2022-09-25 22:18:38 +0900

    Add exceptionPractice

---
+++ dist/chapter5/exceptionPractice.js
@@ -0,0 +1,18 @@
+"use strict";
+var exceptionPractice;
+(function (exceptionPractice) {
+    try {
+        console.log('b');
+        throwError();
+        console.log("aaa");
+    }
+    catch (err) {
+        console.log('catched error');
+        console.log(err);
+    }
+    console.log('end');
+    function throwError() {
+        const error = new Error("has raised error");
+        throw error;
+    }
+})(exceptionPractice || (exceptionPractice = {}));
