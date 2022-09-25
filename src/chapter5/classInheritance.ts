caf2c72e1d695e2c12765b36dabf2e7c01d822bf
nogtk <taka.naoga@moneyforward.co.jp>
2022-09-25 19:00:05 +0900

    Add classInheritance

---
+++ src/chapter5/classInheritance.ts
@@ -0,0 +1,25 @@
+namespace classInheritance {
+  class User {
+    name: string;
+    #age: number;
+
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.#age = age;
+    }
+
+    public isAdult(): boolean {
+      return this.#age >= 20;
+    }
+  }
+
+  class PremiumUser extends User {
+    rank: number = 1;
+
+    public override isAdult(): boolean {
+      return true;
+    }
+  }
+
+  console.log(new PremiumUser("test", 1).isAdult());
+}
